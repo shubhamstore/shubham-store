54be9930866aeb65e64bf501443645f5d99ce5bd
shubhammaurya1628 <shubhammaurya1628@gmail.com>
2026-05-08 07:03:39 +0530

    Added admin password

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, Response
 import json
 import os
 
 app = Flask(__name__)
 
-# Products load karne ka function
+# Security ke liye Username aur Password
+USER = "shubham"
+PASS = "162830"
+
+def check_auth(username, password):
+    return username == USER and password == PASS
+
+def authenticate():
+    return Response(
+    'Login Required!', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 def get_products():
     if not os.path.exists('products.json'):
         with open('products.json', 'w') as f:
@@ -19,6 +30,10 @@ def index():
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
+    auth = request.authorization
+    if not auth or not check_auth(auth.username, auth.password):
+        return authenticate()
+    
     if request.method == 'POST':
         new_product = {
             "name": request.form.get('name'),
@@ -34,4 +49,3 @@ def admin():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
